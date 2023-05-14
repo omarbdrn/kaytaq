5ebe9907d8f1ab8e55bd367acdd6188da56bc6fb
Omar Badran <32434709+omarbdrn@users.noreply.github.com>
2023-05-14 20:36:54 +0300

    CODERUB: Making the code pretty nothing important

--- main.py
+++ main.py
@@ -3,11 +3,22 @@ import json
 import feedparser
 from bs4 import BeautifulSoup
 
-# Todo: Add InoReader instead of different RSS feeds
-
 class KayTaq:
     def __init__(self):
-        self.rss_feeds = {"SecLists FullDisclosure": {"url":"https://seclists.org/rss/fulldisclosure.rss", "parser": "SecLists"}, "SecLists Pentest": {"url":"https://seclists.org/rss/pen-test.rss", "parser": "SecLists"}, "Reddit": {"url":"https://www.reddit.com/r/netsec/.rss", "parser": "Reddit"}}
+        self.rss_feeds = {
+            "SecLists FullDisclosure": {
+                "url": "https://seclists.org/rss/fulldisclosure.rss",
+                "parser": "SecLists"
+            },
+            "SecLists Pentest": {
+                "url": "https://seclists.org/rss/pen-test.rss",
+                "parser": "SecLists"
+            },
+            "Reddit": {
+                "url": "https://www.reddit.com/r/netsec/.rss",
+                "parser": "Reddit"
+            }
+        }
         self.entries = 0
         self.snippet_size = 4096
         self.blogBuilderAPI = "http://127.0.0.1:8000/api/models/run?service=blogs&blogType=unique&channel={channel}"
@@ -15,27 +26,27 @@ class KayTaq:
 
     def open_channel(self, channel):
         response = requests.get(self.blogBuilderAPI.format(channel=channel))
-        if response.json()['success'] == True:
+        if response.json()['success']:
             return True
         else:
             raise Exception(response.json()['errors'])
-    
+
     def send_to_channel(self, channel, chunk):
         response = requests.post(self.blogBuilderAPI.format(channel=channel), json={"query": chunk})
-        if response.json()['success'] == True:
+        if response.json()['success']:
             return True
         else:
             raise Exception(response.json()['errors'])
-    
+
     def get_final_blog(self, channel):
         response = requests.post(self.blogBuilderAPI.format(channel=channel), json={"status": "Finished"})
-        if response.json()['success'] == True:
+        if response.json()['success']:
             return response.json()['blog']
         else:
             raise Exception(response.json()['errors'])
 
     def split_text(self, content, chunk_size):
-        num_snippets = (len(content) + chunk_size - 1) // chunk_size 
+        num_snippets = (len(content) + chunk_size - 1) // chunk_size
         snippets = []
         for i in range(num_snippets):
             start = i * chunk_size
@@ -48,7 +59,7 @@ class KayTaq:
         for feed_name, feed_info in self.rss_feeds.items():
             feed_url = feed_info["url"]
             feed_parser = feed_info["parser"]
-            
+
             feed = feedparser.parse(feed_url)
             for entry in feed.entries:
                 blog_title = entry.title
@@ -57,29 +68,27 @@ class KayTaq:
                 blog_summary = soup.get_text()
                 self.parseFeed(blog_link, feed_parser)
                 self.entries += 1
-    
+
     def parseFeed(self, url, feed_parser):
-        # Open Channel to Model
         self.open_channel(self.currentFeed)
 
-        # Get Feed Text
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
-        if feed_parser == "Reddit":    
+        
+        if feed_parser == "Reddit":
             div = soup.find("div", {"data-post-click-location": "post-media-cta"})
-            if div != None:
-                items = ''.join([line for line in div.get_text().split("\n") if line.strip != ''])
+            if div is not None:
+                items = ''.join([line for line in div.get_text().split("\n") if line.strip()])
                 if len(items) > 48:
-                    # If Greater than 48 Characters split context into multiple chunks
                     for chunk in self.split_text(items, self.snippet_size):
                         print(chunk)
         elif feed_parser == "SecLists":
             data = soup.get_text()
-            items = ' '.join([line for line in data.split("\n") if line.strip != ''])
+            items = ' '.join([line for line in data.split("\n") if line.strip()])
             if len(items) > 48:
                 for chunk in self.split_text(items, self.snippet_size):
                     self.send_to_channel(self.currentFeed, chunk)
-        
+
         print(self.get_final_blog(self.currentFeed))
         self.currentFeed += 1
 
